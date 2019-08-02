e6faf5530a930fa337f767fbb0caffd0bbc129bf
Regisha08 <regishan.08@gmail.com>
2019-08-02 09:59:05 -0700

    add change

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: rnureeva <rnureeva@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/29 22:28:59 by rnureeva          #+#    #+#             */
-/*   Updated: 2019/08/02 09:49:17 by rnureeva         ###   ########.fr       */
+/*   Updated: 2019/08/02 09:58:49 by rnureeva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ char	*ft_strdup(const char *s1)
 	char	*res;
 	int		i;
 
-	if ((res = (char *)malloc(sizeof(char) * (ft_strlen(s1) + 1))))
+	res = (char *)malloc(ft_strlen(s1) + 1);
+	if (res == NULL)
 		return (NULL);
 	i = 0;
 	while (s1[i])
